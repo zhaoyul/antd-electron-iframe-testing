a18ecacecba2b962af4538779b594d42de144040
kevin li <lizhoayu1@redcreation.cn>
2025-07-11 11:21:23 +0800

    [feat] 随机弹窗(model/alert)

--- app/antd-electron-iframe-testing/src/iframe.tsx
+++ app/antd-electron-iframe-testing/src/iframe.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Modal } from 'antd';
+import { Modal, Alert } from 'antd';
 
 const App = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isAlertVisible, setIsAlertVisible] = useState(false);
   const id = new URLSearchParams(window.location.search).get('id');
 
   const showModal = () => {
@@ -19,12 +20,36 @@ const App = () => {
   };
 
   useEffect(() => {
-    // Show the modal right away
-    setTimeout(showModal, 500);
+    const modalInterval = setInterval(() => {
+      if (Math.random() > 0.5) {
+        setIsModalVisible(true);
+      }
+    }, 1000);
+
+    const alertInterval = setInterval(() => {
+      if (Math.random() > 0.5) {
+        setIsAlertVisible(true);
+        setTimeout(() => setIsAlertVisible(false), 2000);
+      }
+    }, 700);
+
+    return () => {
+      clearInterval(modalInterval);
+      clearInterval(alertInterval);
+    };
   }, []);
 
   return (
     <div>
+      {isAlertVisible && (
+        <Alert
+          message={`Alert from iframe ${id}`}
+          type="warning"
+          showIcon
+          closable
+          onClose={() => setIsAlertVisible(false)}
+        />
+      )}
       <Modal title={`Modal from iframe ${id}`} open={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
         <p>Some contents...</p>
         <p>Some contents...</p>
@@ -37,4 +62,3 @@ const App = () => {
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<App />);
-
